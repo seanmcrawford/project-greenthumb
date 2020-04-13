a89c996b3d353488123e316424e189f402fe4ecd
Sean <seanmcrawford@gmail.com>
2020-04-12 22:52:35 -0400

    Adding demo file
    Co-authored-by: danielpcrawford1 <danielpcrawford1@gmail.com>

---
+++ demo_python.py
@@ -0,0 +1,23 @@
+import time
+
+my_list = ["Sean", "Dan", "Megan"]
+
+print(my_list)
+
+for name in my_list:
+    print(name)
+
+print("Done")
+
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+    time.sleep(1)
+
+
+# Uncomment to run this (Caution: this is an infinite loop)
+# while True:
+#     print(time.time())
+#     time.sleep(1)
